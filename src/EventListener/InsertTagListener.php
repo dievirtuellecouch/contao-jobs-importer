8b095e596706364f43c34b67bc3bd15d501aaa93
Jonathan Hermann <github@dievirtuellecouch.net>
2024-02-02 13:51:31 +0100

    Add insert tag for external application url

---
+++ src/EventListener/InsertTagListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DVC\JobsImporter\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\Input;
+use Plenta\ContaoJobsBasic\Contao\Model\PlentaJobsBasicOfferModel as JobOfferModel;
+
+#[AsHook('replaceInsertTags')]
+class InsertTagListener
+{
+    public const TAG = 'job';
+
+    public function __invoke(string $tag)
+    {
+        $chunks = explode('::', $tag);
+
+        if ($chunks[0] !== self::TAG) {
+            return false;
+        }
+
+        $alias = Input::get('items');
+        $offer = JobOfferModel::findPublishedByIdOrAlias($alias);
+
+        if ($offer === null)
+        {
+            return false;
+        }
+
+        switch ($chunks[1]) {
+            case 'externalApplicationUrl':
+                return $this->getExternalApplciationUrl($offer);
+        }
+
+        return false;
+    }
+
+    private function getExternalApplciationUrl(JobOfferModel $offer): string
+    {
+        return $offer->externalApplicationUrl ?? '';
+    }
+}
